6cc68d640e8ab80ba5462c8f4ffb1ef179040d59
Сергій Овчинніков <61272564+unknownMusician@users.noreply.github.com>
2022-08-30 19:52:35 +0300

    Update Immutable.cs

--- Mutability/Immutable.cs
+++ Mutability/Immutable.cs
@@ -1,6 +1,9 @@
-﻿namespace AreYouFruits.Common.Mutability
+﻿using System;
+using System.Collections.Generic;
+
+namespace AreYouFruits.Common.Mutability
 {
-    public readonly struct Immutable<T>
+    public readonly struct Immutable<T> : IEquatable<Immutable<T>>
         where T : struct
     {
         public readonly T Value;
@@ -10,6 +13,36 @@
             Value = value;
         }
 
+        public bool Equals(Immutable<T> other)
+        {
+            return EqualityComparer<T>.Default.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Immutable<T> other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Value)}: {Value}";
+        }
+
+        public static bool operator ==(Immutable<T> a, Immutable<T> b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Immutable<T> a, Immutable<T> b)
+        {
+            return !a.Equals(b);
+        }
+        
         public static implicit operator Immutable<T>(in T value)
         {
             return new Immutable<T>(in value);
